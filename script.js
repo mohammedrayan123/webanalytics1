5124fc43e95460a6659ceba1ab48a405fe5b6315
mohammedrayan123 <143073504+mohammedrayan123@users.noreply.github.com>
2024-08-05 12:48:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.getElementById('clickMe').addEventListener('click', function() {
     document.getElementById('message').classList.toggle('hidden');
 });
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-400QXYD5PV"></script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-400QXYD5PV');
